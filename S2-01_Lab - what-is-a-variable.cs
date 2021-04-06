73a3c305bd71c1277f0fbdda2102cdcfa18944fe
sabet <sabet@cstructor.com>
2021-04-05 20:56:21 -0700

    Session 2 - Lab Solutions

---
+++ S2-01_Lab - what-is-a-variable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Declare a variable named "age" of type "int"
+        int age;
+
+        // Set the value of "age" to 21
+        age = 21;
+
+        Console.WriteLine("My age is {0}", age);
+
+        age = age + 2; // increment age by 2
+
+        Console.WriteLine("My real age is {0}", age);
+        Console.ReadLine();
+    }
+}
---
+++ S2-Lab - polynomial.cs
@@ -0,0 +1,37 @@
+﻿//datatype in class exercise
+//Calculate the polynomial
+using System;
+
+namespace S2_Lab1_CalculatePolynomial
+{
+    class Polynomial
+    {
+        static void Main()
+        {
+            while (true) // allows to keep trying
+            {
+
+                // Ask for an input 
+                Console.Write("Please type an integer value for x: ");
+
+                // Read the input and save it into a String Type
+                string str = Console.ReadLine();
+
+                //Convert the String Type into an Integer Type
+                int x = int.Parse(str);  // convert a string into an integer type
+
+                // Calculate the Polynomial and Save it into an Integer Type
+                int iResult = (int)((3 * x * x * x) - (5 * x * x) + 6);
+
+                // Show the result on the Console (on the screen)
+                Console.WriteLine("The calculated value for (3x^3)-(5x^2)+6 is {0}", iResult);
+
+                Console.WriteLine(); // an extra line
+
+                // Hold the Console so we can see the result
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
