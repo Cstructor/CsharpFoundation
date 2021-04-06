dde37bbf9810b4b5d5320955138aa991acdcb4bb
sabet <sabet@cstructor.com>
2021-04-05 20:54:17 -0700

    Sample HelloWorld

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while(true)
+        { 
+        // Ask for an input 
+        Console.Write("Please enter an integer value for x: ");
+
+        // Read the input and save it into a String Type
+        string input = Console.ReadLine(); 
+
+        // Convert the String Type into an Integer Type
+        int x = int.Parse(input);  // convert a string into an integer type
+
+        // Calculate the Polynomial (3x^3-5x^2+6) and save it into an Integer Type
+        int result = (3 * x * x * x) - (5 * x * x) + 6;
+
+        // Show the result on the Console (on the screen)
+        Console.WriteLine("The calculated value for 3x^3-5x^2+6 is {0}", result);
+
+        Console.WriteLine(); // an extra line
+
+        // Hold the Console so we can see the result
+        Console.ReadLine();
+        }
+    }
+
+}
